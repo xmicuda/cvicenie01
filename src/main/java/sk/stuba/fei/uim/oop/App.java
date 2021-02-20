b79b608fa2b9576682fc0774ee7a380cc84b4221
Micuda Roman <micuda.roman@gmail.com>
2021-02-20 07:46:14 +0100

    Counting the number of characters

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 
 public class App{
     public static void main(String[] args){
-        System.out.print("What is your name? ");
+        System.out.print("What is the input string? ");
 
         Scanner input = new Scanner(System.in);
         String name = input.next();
-        System.out.print("Hello, "+name+", nice to meet you!");
+        System.out.print(name+" has "+name.length()+" characters.");
 
     }
 }
