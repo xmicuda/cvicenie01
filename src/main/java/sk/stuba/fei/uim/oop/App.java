badcf898e05cca7c284707e18bb575a23e4cab87
Micuda Roman <micuda.roman@gmail.com>
2021-02-20 07:42:09 +0100

    Hello world + greeting

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -1,7 +1,13 @@
 package sk.stuba.fei.uim.oop;
+import java.util.Scanner;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+public class App{
+    public static void main(String[] args){
+        System.out.print("What is your name? ");
+
+        Scanner input = new Scanner(System.in);
+        String name = input.next();
+        System.out.print("Hello, "+name+", nice to meet you!");
+
     }
 }
